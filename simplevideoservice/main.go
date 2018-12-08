64f359a691f4b9b163b60bfbcd9adf5d19b29aa6
Alexey Malov <malov.aleksey@gmail.com>
2018-12-08 11:44:15 +0300

    Добавлено логирование

--- simplevideoservice/main.go
+++ simplevideoservice/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"github.com/alexey-malov/gocourse/simplevideoservice/handlers"
 	"net/http"
+	"os"
 )
 
+import log "github.com/sirupsen/logrus"
+
+func setupLogger() (*os.File, error) {
+	log.SetFormatter(&log.JSONFormatter{})
+	file, err := os.OpenFile("my.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(file)
+	}
+	return file, err
+}
+
 func main() {
+	if file, _ := setupLogger(); file != nil {
+		defer func() {
+			_ = file.Close()
+		}()
+	} else {
+		log.Fatal("Failed to create log")
+	}
+
+	serverUrl := ":8000"
+	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
+
 	router := handlers.Router()
-	fmt.Println(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", router))
+
 }
