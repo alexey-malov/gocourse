1116dfed97921350f720a1acbd308f3e39bf5591
Alexey Malov <malov.aleksey@gmail.com>
2018-12-08 11:21:14 +0300

    Выпилил лишний код

--- simplevideoservice/main.go
+++ simplevideoservice/main.go
@@ -9,36 +9,4 @@ import (
 func main() {
 	router := handlers.Router()
 	fmt.Println(http.ListenAndServe(":8000", router))
-	/*
-		http.HandleFunc("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
-			_, err := fmt.Fprint(w, "Hello world!")
-			if err != nil {
-
-			}
-		})
-		http.HandleFunc("/api/v1/list", func(w http.ResponseWriter, _ *http.Request) {
-			_, err := fmt.Fprintf(w, `[{
-	    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-	    "name": "Black Retrospetive Woman",
-	    "duration": 15,
-	    "thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
-	}]`)
-			if err != nil {
-
-			}
-		})
-		http.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", func(w http.ResponseWriter, _ *http.Request) {
-			_, err := fmt.Fprint(w, `{
-	    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-	    "name": "Black Retrospetive Woman",
-	    "duration": 15,
-	    "thumbnail":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
-	    "url":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4"
-	}`)
-			if err != nil {
-
-			}
-		})
-		http.ListenAndServe(":8000", nil)
-	*/
 }
